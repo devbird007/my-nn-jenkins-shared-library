56d5e0493115804d15cb6848c1bb83fed6b37aa4
Emmanuel Taiwo <100073682+devbird007@users.noreply.github.com>
2025-03-07 11:44:39 -0600

    Fix syntax error

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,6 +1,6 @@
 def call() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'dockerhub-repo', passwoordVariable: 'PASS', usernameVariable: 'USER')]) {
+    withCredentials([usernamePassword(credentialsId: 'dockerhub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t mannyops/pipeline-demo-app:groovy1.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push mannyops/pipeline-demo-app:groovy1.0'
